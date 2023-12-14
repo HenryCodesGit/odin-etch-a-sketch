e6c03ec556a2ec165fd8d6dbba5c20eba9ccc0dc
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2023-12-13 22:32:17 -0500

    Complete the project

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log('hello world!');
+
+const GRID_SIZE = 16;
+const CONTAINER = document.querySelector('.container');
+const GRID_COUNT_BUTTON = document.querySelector('#grid-count');
+GRID_COUNT_BUTTON.addEventListener("click", () => makeNewGrid());
+
+makeRows(GRID_SIZE);
+
+function makeRows(size){
+    for(i = 0; i<size; i++){
+
+        let currRow = document.createElement('div');
+        currRow.classList.add('row');
+        for(j = 0; j<size; j++){
+            let currCell = document.createElement('div');
+            currCell.classList.add('cell');
+            currRow.appendChild(currCell);
+    
+            currCell.addEventListener("mouseenter", ()=>{darkenCell(currCell);});
+        }
+    
+        CONTAINER.appendChild(currRow);
+    }
+}
+
+function darkenCell(element){
+    element.style.backgroundColor = '#000000';
+}
+
+function makeNewGrid(){
+    let count = prompt('New Grid Size:');
+    
+    for(i = CONTAINER.childNodes.length -1; i>=0; i--){
+        CONTAINER.removeChild(CONTAINER.childNodes[i]);
+    }
+
+    makeRows(count);
+}
